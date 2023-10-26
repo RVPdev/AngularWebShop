7fd1b59b8dfe768ceef9e773b6cc3e1c9ae44f09
RVPdev <renatovilcahuaman@gmail.com>
2023-10-26 15:45:54 -0500

    added sorter sectuin and basic code

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+// Import necessary Angular modules
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
+// Component metadata
 @Component({
-  selector: 'app-products-header',
-  templateUrl: 'products-header.component.html',
+  selector: "app-products-header", // Component selector
+  templateUrl: "products-header.component.html", // Template URL
 })
+// Component class definition
 export class ProductsHeaderComponent implements OnInit {
+  
+  // Output event emitter for column count changes
+  @Output() columsCountChange = new EventEmitter<number>();
+
+  // Default sorting order
   sort = "desc";
+  
+  // Default number of items to show
+  itemShowCount = 12;
 
-  constructor() { }
+  // Constructor (currently empty)
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  // OnInit lifecycle hook (currently empty)
+  ngOnInit(): void {}
 
+  // Method to update sorting order
   onSortUpdate(newSort: string): void {
     this.sort = newSort;
   }
 
+  // Method to update the number of items to show
+  onItemsUpdated(count: number): void {
+    this.itemShowCount = count;
+  }
+
+  // Method to update the number of columns to display
+  onColumnsUpdated(colsNumber: number): void {
+    // Emit the new column count
+    this.columsCountChange.emit(colsNumber);
+  }
 }
