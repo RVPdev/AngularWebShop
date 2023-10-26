16dcbf206d28c73e9d155e5ca95770aef5411672
RVPdev <renatovilcahuaman@gmail.com>
2023-10-26 17:26:29 -0500

    added commnets

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,19 +1,29 @@
+// Import the necessary Angular modules
 import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
+// Component metadata
 @Component({
-  selector: "app-filters",
-  templateUrl: "filters.component.html",
+  selector: "app-filters", // Component's selector
+  templateUrl: "filters.component.html", // Template URL
 })
+// Component class definition
 export class FiltersComponent implements OnInit {
 
+  // Output event emitter for showing categories
   @Output() showCategory = new EventEmitter<string>();
+
+  // Array of categories to filter by
   categories = ["shoes", "sports"];
 
+  // Constructor (currently empty)
   constructor() {}
 
+  // OnInit lifecycle hook (currently empty)
   ngOnInit(): void {}
 
+  // Method to emit the selected category
   onShowCategory(category: string): void {
+    // Emit the selected category
     this.showCategory.emit(category);
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,33 @@
+// Import the necessary Angular modules
 import { Component, OnInit } from "@angular/core";
 
+// Component metadata
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styles: [],
+  selector: "app-home", // Component's selector
+  templateUrl: "./home.component.html", // Template URL
+  styles: [], // Inline styles (currently empty)
 })
+// Component class definition
 export class HomeComponent implements OnInit {
+
+  // Number of columns to display
   cols = 3;
+
+  // Current category to show (can be undefined)
   category: string | undefined;
 
+  // Constructor (currently empty)
   constructor() {}
 
+  // OnInit lifecycle hook (currently empty)
   ngOnInit(): void {}
 
+  // Method to update the number of columns
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
   }
 
+  // Method to update the category to show
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
   }
