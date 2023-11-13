f0200af6af023b89f628efee9836cee6c0020d42
RVPdev <renatovilcahuaman@gmail.com>
2023-11-12 18:57:15 -0600

    added fcuntionality to cart badge

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Cart } from "src/app/models/cart.model";
 
 @Component({
-  selector: 'app-header',
-  templateUrl:'./header.component.html',
+  selector: "app-header",
+  templateUrl: "./header.component.html",
 })
 export class HeaderComponent implements OnInit {
+  private _cart: Cart = { items: [] };
 
-  constructor() { }
+  itemsQuantity = 0;
 
-  ngOnInit(): void {
+  @Input()
+  get cart(): Cart {
+    return this._cart;
   }
 
+  set cart(cart: Cart) {
+    this._cart = cart;
+    this.itemsQuantity = cart.items
+      .map((item) => item.quantity)
+      .reduce((prev, cur) => prev + cur, 0);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
