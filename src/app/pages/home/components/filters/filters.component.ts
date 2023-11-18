765cef42b2a52b097a5c39e9e753bbdf05b007d9
RVPdev <renatovilcahuaman@gmail.com>
2023-11-17 23:45:44 -0600

    added categories service

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,5 +1,13 @@
 // Import the necessary Angular modules
-import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { Subscription } from "rxjs";
+import { StoreService } from "src/app/services/store.service";
 
 // Component metadata
 @Component({
@@ -7,23 +15,36 @@ import { Component, EventEmitter, OnInit, Output } from "@angular/core";
   templateUrl: "filters.component.html", // Template URL
 })
 // Component class definition
-export class FiltersComponent implements OnInit {
-
+export class FiltersComponent implements OnInit, OnDestroy {
   // Output event emitter for showing categories
   @Output() showCategory = new EventEmitter<string>();
 
+  categoriesSubscription: Subscription | undefined;
+
   // Array of categories to filter by
-  categories = ["shoes", "sports"];
+  categories: Array<string> | undefined;
 
   // Constructor (currently empty)
-  constructor() {}
+  constructor(private storeService: StoreService) {}
 
   // OnInit lifecycle hook (currently empty)
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService
+      .getAllCategories()
+      .subscribe((response) => {
+        this.categories = response;
+      });
+  }
 
   // Method to emit the selected category
   onShowCategory(category: string): void {
     // Emit the selected category
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 }
